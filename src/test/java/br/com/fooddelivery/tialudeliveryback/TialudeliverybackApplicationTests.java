ee7a3896e220d03e4df49ca578cb711924436705
lasilva <lucas.almeida.las@gmail.com>
2025-10-26 13:42:21 -0300

    DWOO-009-48:  implementação da task #48

--- src/test/java/br/com/fooddelivery/tialudeliveryback/TialudeliverybackApplicationTests.java
+++ src/test/java/br/com/fooddelivery/tialudeliveryback/TialudeliverybackApplicationTests.java
@@ -2,8 +2,10 @@ package br.com.fooddelivery.tialudeliveryback;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class TialudeliverybackApplicationTests {
 
 	@Test
