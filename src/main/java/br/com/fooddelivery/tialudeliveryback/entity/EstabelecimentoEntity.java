3af3fbab7a2ed3896b6f2c56b6b1f2cc2388bde3
Erick <persona0rcc@gmail.com>
2025-11-05 18:59:23 -0300

    feat: EstabelecimentoEntity

---
+++ src/main/java/br/com/fooddelivery/tialudeliveryback/entity/EstabelecimentoEntity.java
@@ -0,0 +1,51 @@
+package br.com.fooddelivery.tialudeliveryback.entity;
+
+import jakarta.persistence.*;
+import org.springframework.beans.BeanUtils;
+
+import java.util.Objects;
+
+@Entity
+@Table(name = "ESTABELECIMENTO")
+public class EstabelecimentoEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String nomeFantasia;
+
+    //-------
+
+    public Long getId() {
+        return id;
+    }
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String  getNomeFantasia() {
+        return nomeFantasia;
+    }
+    public void setNomeFantasia(String nomeFantasia) {
+        this.nomeFantasia = nomeFantasia;
+    }
+
+    //-----------------------
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) return true;
+        if (obj == null) return false;
+        if (getClass() != obj.getClass()) return false;
+
+        EstabelecimentoEntity other = (EstabelecimentoEntity) obj;
+        return Objects.equals(id, other.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
+}
