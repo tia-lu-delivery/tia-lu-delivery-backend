c6c7cb963feedd9b354501db82b2d84d12c9def1
Erick <persona0rcc@gmail.com>
2025-11-05 16:35:53 -0300

    feat: hashcode e equals

--- src/main/java/br/com/fooddelivery/tialudeliveryback/entity/CardapioEntity.java
+++ src/main/java/br/com/fooddelivery/tialudeliveryback/entity/CardapioEntity.java
@@ -2,6 +2,8 @@ package br.com.fooddelivery.tialudeliveryback.entity;
 
 import jakarta.persistence.*;
 
+import java.util.Objects;
+
 @Entity
 @Table(name = "CARDAPIO")
 public class CardapioEntity {
@@ -27,6 +29,22 @@ public class CardapioEntity {
 
     public String getDataAtualizacao() {
         return dataAtualizacao;
+    }
+
+    //---------
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) return true;
+        if (obj == null) return false;
+        if (getClass() != obj.getClass()) return false;
+
+        CardapioEntity other = (CardapioEntity) obj;
+        return Objects.equals(id, other.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
     }
 
 }
