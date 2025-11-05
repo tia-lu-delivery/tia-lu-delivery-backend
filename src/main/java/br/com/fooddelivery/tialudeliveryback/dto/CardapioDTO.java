3d6ea20e5be3f625b511a18dfacf5ae340d0f0da
Erick <persona0rcc@gmail.com>
2025-11-05 16:48:22 -0300

    feat: get e set dto Cardapio

--- src/main/java/br/com/fooddelivery/tialudeliveryback/dto/CardapioDTO.java
+++ src/main/java/br/com/fooddelivery/tialudeliveryback/dto/CardapioDTO.java
@@ -1,4 +1,36 @@
 package br.com.fooddelivery.tialudeliveryback.dto;
 
+
 public class CardapioDTO {
+
+    private Long id;
+
+    private String nome;
+
+    private String dataAtualizacao;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getDataAtualizacao() {
+        return dataAtualizacao;
+    }
+
+    public void setDataAtualizacao(String dataAtualizacao) {
+        this.dataAtualizacao = dataAtualizacao;
+    }
+
 }
