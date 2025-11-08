f6eb0773020f03c2bff444985a69a52bb885c53f
Erick <persona0rcc@gmail.com>
2025-11-08 10:20:20 -0300

    feat: Atributos dto para menu

--- src/main/java/br/com/fooddelivery/tialudeliveryback/dto/MenuDTO.java
+++ src/main/java/br/com/fooddelivery/tialudeliveryback/dto/MenuDTO.java
@@ -1,4 +1,14 @@
 package br.com.fooddelivery.tialudeliveryback.dto;
 
+import java.util.List;
+
 public class MenuDTO {
+
+    private String idCardapio;
+    private String nomeCardapio;
+    private String dataAtualizacao;
+    private EstabelecimentoDTO estabelecimento;
+    private List<CategoriaDTO> categorias;
+    private String mensagem;
+
 }
