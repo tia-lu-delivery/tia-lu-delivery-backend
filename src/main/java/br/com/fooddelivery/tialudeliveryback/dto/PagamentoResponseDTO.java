c833a49e69050eee07a6a9b2ad3001721422c275
pedrocesr <pcixao09@gmail.com>
2025-11-08 15:39:09 -0300

    feat(DTO): Conclui a task #49, criando DTOs de Request e Response para Pagamento.

---
+++ src/main/java/br/com/fooddelivery/tialudeliveryback/dto/PagamentoResponseDTO.java
@@ -0,0 +1,23 @@
+package br.com.fooddelivery.tialudeliveryback.dto;
+
+import lombok.Builder;
+import lombok.Data;
+import java.math.BigDecimal;
+
+@Data
+@Builder
+public class PagamentoResponseDTO {
+    private String idPedido;
+    private String mensagem;
+    private String statusPedidoAtual;
+    private DetalhesPagamento detalhesPagamento;
+
+    @Data
+    @Builder
+    public static class DetalhesPagamento {
+        private String status;
+        private String idTransacao;
+        private BigDecimal valorTotalCobrado;
+        private String meioPagamentoUsado;
+    }
+}
