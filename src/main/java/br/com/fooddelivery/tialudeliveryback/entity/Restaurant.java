5fc6e275c9d722bdedd38376378fdce217d2b8a8
HeyAlexOliveira <135489596+HeyAlexOliveira@users.noreply.github.com>
2025-11-08 20:04:04 -0300

    Create Restaurant.java

---
+++ src/main/java/br/com/fooddelivery/tialudeliveryback/entity/Restaurant.java
@@ -0,0 +1,26 @@
+package br.com.fooddelivery.tialudeliveryback.entity;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "restaurants")
+public class Restaurant {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, length = 100)
+    private String name;
+
+    public Restaurant() {}
+
+    public Restaurant(String name) {
+        this.name = name;
+    }
+
+    public Long getId() { return id; }
+
+    public String getName() { return name; }
+    public void setName(String name) { this.name = name; }
+}
