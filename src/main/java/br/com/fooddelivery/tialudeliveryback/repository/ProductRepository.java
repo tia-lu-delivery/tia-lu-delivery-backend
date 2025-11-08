474e3a61621d312c649244d54021e9a9f535e508
HeyAlexOliveira <135489596+HeyAlexOliveira@users.noreply.github.com>
2025-11-08 20:02:34 -0300

    Refactor method signature for findByRestaurantIdAndProductId

--- src/main/java/br/com/fooddelivery/tialudeliveryback/repository/ProductRepository.java
+++ src/main/java/br/com/fooddelivery/tialudeliveryback/repository/ProductRepository.java
@@ -2,14 +2,11 @@ package br.com.fooddelivery.tialudeliveryback.repository;
 
 import br.com.fooddelivery.tialudeliveryback.entity.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
 @Repository
 public interface ProductRepository extends JpaRepository<Product, Long> {
-
-    @Query("SELECT p FROM Product p WHERE p.restaurant.id = :idRestaurante AND p.id = :idProduto")
-    Optional<Product> findByRestaurantIdAndProductId(Long idRestaurante, Long idProduto);
+    Optional<Product> findByRestaurantIdAndProductId(Long restaurantId, Long productId);
 }
