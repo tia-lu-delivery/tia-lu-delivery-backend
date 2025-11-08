6beed192ee4014e3bc89c96d3d31dea3d9a9eb4f
Erick <persona0rcc@gmail.com>
2025-11-08 10:20:55 -0300

    feat: Anotações

--- src/main/java/br/com/fooddelivery/tialudeliveryback/dto/MenuDTO.java
+++ src/main/java/br/com/fooddelivery/tialudeliveryback/dto/MenuDTO.java
@@ -1,7 +1,16 @@
 package br.com.fooddelivery.tialudeliveryback.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
 import java.util.List;
 
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class MenuDTO {
 
     private String idCardapio;
