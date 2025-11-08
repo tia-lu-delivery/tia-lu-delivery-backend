d932adcd678e9041dd76a982b661a111eeb27511
HeyAlexOliveira <135489596+HeyAlexOliveira@users.noreply.github.com>
2025-11-07 23:20:25 -0300

    Delete src/main/java/br/com/fooddelivery/tialudeliveryback/repository directory

--- src/main/java/br/com/fooddelivery/tialudeliveryback/repository/ProductRepository.java
+++
@@ -1,9 +0,0 @@
-package br.com.fooddelivery.tialudeliveryback.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-import br.com.fooddelivery.tialudeliveryback.entity.Product;
-
-@Repository
-public interface ProductRepository extends JpaRepository<Product, Long> {
-}
