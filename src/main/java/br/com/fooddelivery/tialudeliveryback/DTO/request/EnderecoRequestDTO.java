bc498dd3cde0c4719e44ab1b643cdefa502e12a6
Igor <igorvieiraart3@gmail.com>
2025-11-07 23:22:00 -0300

    Implementa EnderecoRequestDTO e EnderecoResponseDTO - DW00-12-5

---
+++ src/main/java/br/com/fooddelivery/tialudeliveryback/DTO/request/EnderecoRequestDTO.java
@@ -0,0 +1,72 @@
+package com.tialu.delivery.dto.request;
+
+public class EnderecoRequestDTO {
+    private String rua;
+    private String numero;
+    private String bairro;
+    private String cidade;
+    private String estado;
+    private String cep;
+    private String complemento;
+
+    // Construtor padrão
+    public EnderecoRequestDTO() {
+    }
+
+    // Getters e Setters
+    public String getRua() {
+        return rua;
+    }
+
+    public void setRua(String rua) {
+        this.rua = rua;
+    }
+
+    public String getNumero() {
+        return numero;
+    }
+
+    public void setNumero(String numero) {
+        this.numero = numero;
+    }
+
+    public String getBairro() {
+        return bairro;
+    }
+
+    public void setBairro(String bairro) {
+        this.bairro = bairro;
+    }
+
+    public String getCidade() {
+        return cidade;
+    }
+
+    public void setCidade(String cidade) {
+        this.cidade = cidade;
+    }
+
+    public String getEstado() {
+        return estado;
+    }
+
+    public void setEstado(String estado) {
+        this.estado = estado;
+    }
+
+    public String getCep() {
+        return cep;
+    }
+
+    public void setCep(String cep) {
+        this.cep = cep;
+    }
+
+    public String getComplemento() {
+        return complemento;
+    }
+
+    public void setComplemento(String complemento) {
+        this.complemento = complemento;
+    }
+}
