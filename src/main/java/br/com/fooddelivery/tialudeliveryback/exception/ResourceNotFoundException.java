893a851a5ae0b44ca93366fa313e7f24a40ec5d1
HeyAlexOliveira <135489596+HeyAlexOliveira@users.noreply.github.com>
2025-11-07 23:20:13 -0300

    Delete src/main/java/br/com/fooddelivery/tialudeliveryback/exception directory

--- src/main/java/br/com/fooddelivery/tialudeliveryback/exception/ResourceNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package br.com.fooddelivery.tialudeliveryback.exception;
-
-public class ResourceNotFoundException extends RuntimeException {
-    public ResourceNotFoundException(String message) {
-        super(message);
-    }
-}
