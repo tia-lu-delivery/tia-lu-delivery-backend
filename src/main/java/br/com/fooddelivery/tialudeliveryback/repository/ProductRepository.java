90b05131e00c382033b5ce5e6aedc63f981cf5f1
HeyAlexOliveira <135489596+HeyAlexOliveira@users.noreply.github.com>
2025-11-08 19:53:23 -0300

    Update ProductRepository.java

--- src/main/java/br/com/fooddelivery/tialudeliveryback/repository/ProductRepository.java
+++ src/main/java/br/com/fooddelivery/tialudeliveryback/repository/ProductRepository.java
@@ -1,15 +1,15 @@
 package br.com.fooddelivery.tialudeliveryback.repository;
 
+import br.com.fooddelivery.tialudeliveryback.entity.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
-import br.com.fooddelivery.tialudeliveryback.entity.Product;
-import java.util.List;
+
+import java.util.Optional;
 
 @Repository
 public interface ProductRepository extends JpaRepository<Product, Long> {
 
-    // Busca produtos pelo nome (parcial, ignorando maiúsculas/minúsculas)
-    @Query("SELECT p FROM Product p WHERE LOWER(p.name) LIKE LOWER(CONCAT('%', :name, '%'))")
-    List<Product> findByNameContainingIgnoreCase(String name);
+    @Query("SELECT p FROM Product p WHERE p.restaurant.id = :idRestaurante AND p.id = :idProduto")
+    Optional<Product> findByRestaurantIdAndProductId(Long idRestaurante, Long idProduto);
 }
