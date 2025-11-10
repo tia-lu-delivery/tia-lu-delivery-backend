256228afc8e22d406d691084ea941123cd77ab2d
Tiago <106884372+tiagosangil@users.noreply.github.com>
2025-11-10 11:52:18 -0300

    Update OwnerPartnerRequestDTO.java

--- src/main/java/br/com/fooddelivery/tialudeliveryback/dto/OwnerPartnerRequestDTO.java
+++ src/main/java/br/com/fooddelivery/tialudeliveryback/dto/OwnerPartnerRequestDTO.java
@@ -1,8 +1,27 @@
+package br.com.fooddelivery.tialudeliveryback.dto;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
 public class OwnerPartnerRequestDTO {
 
     private Stringcpf;
+    @NotBlank(message = "CPF não pode ser nulo ou vazio.")
+    @Pattern(regexp = "\\d{11}", message = "CPF deve conter exatamente 11 dígitos.")
+    private String cpf;
 
     private Stringrg;
+    @NotBlank(message = "RG não pode ser nulo ou vazio.")
+    private String rg;
 
     private StringOrgaoEmissorRg;
 
@@ -11,7 +30,16 @@ public class OwnerPartnerRequestDTO {
     private StringTelefone;
 
     private StringNomeCompleto;
+    @NotBlank(message = "Órgão emissor do RG não pode ser nulo ou vazio.")
+    private String orgaoEmissorRg;
 
+    @NotBlank(message = "Email não pode ser nulo ou vazio.")
+    @Email(message = "O formato do email é inválido.")
+    private String email;
 
+    @NotBlank(message = "Telefone não pode ser nulo ou vazio.")
+    private String telefone;
 
+    @NotBlank(message = "Nome completo não pode ser nulo ou vazio.")
+    private String nomeCompleto;
 }
