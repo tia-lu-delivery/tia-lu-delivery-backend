14cba826f08bc84bff8339b99ff2ad36e6fd8e5a
Vinicius de Oliveira Costa <113479302+iYzor@users.noreply.github.com>
2025-11-11 16:36:21 -0300

    Update GlobalExceptionHandler.java
    Alterações solicitadas no code review foram realizadas.

--- src/main/java/br/com/fooddelivery/tialudeliveryback/exception/GlobalExceptionHandler.java
+++ src/main/java/br/com/fooddelivery/tialudeliveryback/exception/GlobalExceptionHandler.java
@@ -4,15 +4,18 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import br.com.fooddelivery.tialudeliveryback.dto.ErrorDTO;
 
+@RestControllerAdvice
 public class GlobalExceptionHandler {
-
+    
+    @ExceptionHandler(ResourceNotFoundException.class)
     public ResponseEntity<ErrorDTO> handleResourceNotFoundException(ResourceNotFoundException ex) {
         ErrorDTO error = new ErrorDTO();
         error.setCodigoErro("RESOURCE_NOT_FOUND");
         error.setMensagem(ex.getMessage());
         return ResponseEntity.status(HttpStatus.NOT_FOUND).body(error);
     }
-
+    
+    @ExceptionHandler(Exception.class)
     public ResponseEntity<ErrorDTO> handleGenericException(Exception ex) {
         ErrorDTO error = new ErrorDTO();
         error.setCodigoErro("INTERNAL_SERVER_ERROR");
