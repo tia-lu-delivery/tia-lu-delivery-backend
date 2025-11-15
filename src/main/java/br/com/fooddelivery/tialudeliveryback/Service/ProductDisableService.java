71bc14c270ff80f234b10a0eb19e72624a6de6db
Arthur De Aquino <Sairth0@gmail.com>
2025-11-15 16:09:07 -0300

    Código atualizado para retornar um ProdutoDTO corretamente

--- src/main/java/br/com/fooddelivery/tialudeliveryback/Service/ProductDisableService.java
+++ src/main/java/br/com/fooddelivery/tialudeliveryback/Service/ProductDisableService.java
@@ -1,6 +1,8 @@
 package br.com.fooddelivery.tialudeliveryback.Service;
 
 import br.com.fooddelivery.tialudeliveryback.Entity.Estabelecimento;
+import br.com.fooddelivery.tialudeliveryback.DTO.ProdutoDTO;
+import br.com.fooddelivery.tialudeliveryback.Mapper.ProdutoMapper;
 import br.com.fooddelivery.tialudeliveryback.Entity.Produto;
 import br.com.fooddelivery.tialudeliveryback.Repository.EstabelecimentoRepository;
 import br.com.fooddelivery.tialudeliveryback.Repository.ProdutoRepository;
@@ -14,19 +16,13 @@ public class ProductDisableService {
     private final ProdutoRepository produtoRepository;
     private final EstabelecimentoRepository estabelecimentoRepository;
 
-    public ProductDisableService(ProdutoRepository produtoRepository, EstabelecimentoRepository estabelecimentoRepository) {
-        this.produtoRepository = produtoRepository;
-        this.estabelecimentoRepository = estabelecimentoRepository;
-    }
-
-
-    public Produto disableProduct(Long idEstabelecimento, Long idProduto){
+    public ProdutoDTO disableProduct(Long idEstabelecimento, Long idProduto){
         Estabelecimento estabelecimento = estabelecimentoRepository.
                 findById(idEstabelecimento).orElseThrow(() -> new RuntimeException("Estabelecimento não encontrado"));
         Produto produto = produtoRepository.
                 findById(idProduto).orElseThrow(() -> new RuntimeException("Produto não encontrado"));
 
-        produto.setDisponivel(false);
-        return produtoRepository.save(produto);
+        Produto salvo = produtoRepository.save(produto);
+        return ProdutoMapper.toDTO(salvo);
     }
 }
