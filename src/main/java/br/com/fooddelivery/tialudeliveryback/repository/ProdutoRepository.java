2e4cdb6c69ae4b97085a6af990b2daf0ffc6cb49
matheusfonsecak <matheuskaick70@gmail.com>
2025-11-16 18:21:05 -0300

    feature/Dwoo-017 084 / Update
    UPDATE - Add @transactional no ProdutoRepository

--- src/main/java/br/com/fooddelivery/tialudeliveryback/repository/ProdutoRepository.java
+++ src/main/java/br/com/fooddelivery/tialudeliveryback/repository/ProdutoRepository.java
@@ -1,19 +1,18 @@
 package br.com.fooddelivery.tialudeliveryback.repository;
 
-import br.com.fooddelivery.tialudeliveryback.repository.model.Produto; 
+import br.com.fooddelivery.tialudeliveryback.repository.model.Produto;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
-// Interface para operações com produtos no banco de dados
 @Repository
 public interface ProdutoRepository extends JpaRepository<Produto, Long> {
 
-    // Marca o produto como indisponível no banco de dados
-    // Retorna 1 se encontrou e atualizou, ou 0 se o produto não foi encontrado
+    @Transactional
     @Modifying
     @Query("UPDATE Produto p SET p.disponivel = false WHERE p.id = :id")
     int marcarComoIndisponivel(@Param("id") Long id);
