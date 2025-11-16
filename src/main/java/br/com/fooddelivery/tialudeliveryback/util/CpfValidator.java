2c29b4f8001356c0db68806f220360037fdad9f1
gabriel <ferrazcoelhorodrigo@gmail.com>
2025-11-15 21:42:39 -0300

    Revert "Feature/dwoo 008 41"

--- src/main/java/br/com/fooddelivery/tialudeliveryback/util/CpfValidator.java
+++
@@ -1,8 +0,0 @@
-package br.com.fooddelivery.tialudeliveryback.util;
-
-public class CpfValidator {
-    public static boolean isValid(String cpf) {
-        if (cpf == null || !cpf.matches("\\d{11}")) return false;
-        return true; 
-    }
-}
