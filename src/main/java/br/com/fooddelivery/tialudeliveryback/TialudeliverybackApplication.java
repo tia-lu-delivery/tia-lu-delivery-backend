bb5ea4be1b249b1a65a1a64c38bbe6ab65b9fc4e
Erick <persona0rcc@gmail.com>
2025-11-20 10:05:05 -0300

    Feat: add @

--- src/main/java/br/com/fooddelivery/tialudeliveryback/TialudeliverybackApplication.java
+++ src/main/java/br/com/fooddelivery/tialudeliveryback/TialudeliverybackApplication.java
@@ -2,8 +2,10 @@ package br.com.fooddelivery.tialudeliveryback;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
+@EnableJpaRepositories(basePackages = "br.com.fooddelivery.tialudeliveryback.repository")
 public class TialudeliverybackApplication {
 
 	public static void main(String[] args) {
