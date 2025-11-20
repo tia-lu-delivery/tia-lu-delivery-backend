c29f2f1074644e43d0dc55716bc7ee54c41bbc9b
lc-cmd <lucas7cn@gmail.com>
2025-11-20 17:52:31 -0300

    Criação de tabela merchant e entity

---
+++ src/main/java/br/com/fooddelivery/tialudeliveryback/entity/Merchant.java
@@ -0,0 +1,28 @@
+package br.com.fooddelivery.tialudeliveryback.entity;
+
+import jakarta.persistence.*;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.AllArgsConstructor;
+
+@Entity
+@Table(name = "tb_merchant") // Especificado na task #155
+@Data // Lombok para getters, setters, toString, etc.
+@NoArgsConstructor
+@AllArgsConstructor
+public class Merchant {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false, unique = true)
+    private String cnpj;
+
+    private String address;
+
+    private Boolean active;
+}
