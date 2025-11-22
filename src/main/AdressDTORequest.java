3cb4ee3b84b9a87c7417ba236a167220c5803fe1
Tiago <106884372+tiagosangil@users.noreply.github.com>
2025-11-21 21:26:45 -0300

    Update AdressDTORequest.java

--- src/main/AdressDTORequest.java
+++ src/main/AdressDTORequest.java
@@ -1,5 +1,15 @@
-package br.com.fooddelivery.tialudeliveryback;
+package com.seuprojeto.api.dto.request;
 
-public class AdressDTORequest {
-    
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import lombok.Data;
+
+@Data
+public class AddressDeleteRequestDTO {
+
+    @NotNull(message = "O ID do usuário que solicitou a exclusão é obrigatório.")
+    private Long idUsuarioExecutor;
+
+    @NotBlank(message = "O motivo da exclusão é obrigatório.")
+    private String motivo;
 }
