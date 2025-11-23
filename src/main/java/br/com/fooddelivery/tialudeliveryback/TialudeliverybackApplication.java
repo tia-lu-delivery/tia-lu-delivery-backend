8007802d1748874b11c0c2ff32a8900851175a9c
jorodrigues0 <joaqsonadm@gmail.com>
2025-11-23 19:15:42 -0300

    Criar/validar a Entity

---
+++ src/main/java/br/com/fooddelivery/tialudeliveryback/TialudeliverybackApplication.java
@@ -0,0 +1,13 @@
+package br.com.fooddelivery.tialudeliveryback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TialudeliverybackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TialudeliverybackApplication.class, args);
+	}
+
+}
