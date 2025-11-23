110ea74d92fa1c99446243cff2ea7da89d5c9982
lucasbarretoj <ljbarreto04@gmail.com>
2025-11-23 20:31:21 -0300

    feature/DWOO-026-171: Implementação do serviço de inativação de conta

--- src/main/java/br/com/fooddelivery/tialudeliveryback/repository/UsuarioRepository.java
+++ src/main/java/br/com/fooddelivery/tialudeliveryback/repository/UsuarioRepository.java
@@ -2,8 +2,12 @@ package br.com.fooddelivery.tialudeliveryback.repository;
 
 import br.com.fooddelivery.tialudeliveryback.model.Usuario;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
 }
+
