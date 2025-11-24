e347a9a13c6a8730ab021aa3e6ce2522f71a1a2a
Bruno Lima <bru.no@outlook.com.br>
2025-11-23 21:24:20 -0300

    feat(address): define interface AddressService com contratos para listagem e atualização de endereço principal

---
+++ src/main/java/br/com/fooddelivery/tialudeliveryback/services/AddressService.java
@@ -0,0 +1,11 @@
+package br.com.fooddelivery.tialudeliveryback.services;
+
+import br.com.fooddelivery.tialudeliveryback.dtos.AddressResponseDTO;
+import br.com.fooddelivery.tialudeliveryback.dtos.SetPrincipalAddressResponseDTO;
+import java.util.List;
+
+public interface AddressService {
+    SetPrincipalAddressResponseDTO setAddressAsPrincipal(Long idEndereco, Long idUsuario);
+
+    List<AddressResponseDTO> listAllAddresses(Long idUsuario);
+}
