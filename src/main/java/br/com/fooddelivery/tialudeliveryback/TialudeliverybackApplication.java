a64e8b1705c7d9269ba2130b5e49057b2c600e17
Bruno Lima <bru.no@outlook.com.br>
2025-11-23 21:21:52 -0300

    chore(app): desabilita autoconfiguração de banco de dados para permitir testes locais sem conexão

--- src/main/java/br/com/fooddelivery/tialudeliveryback/TialudeliverybackApplication.java
+++ src/main/java/br/com/fooddelivery/tialudeliveryback/TialudeliverybackApplication.java
@@ -2,12 +2,21 @@ package br.com.fooddelivery.tialudeliveryback;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication
+/**
+ * ⚠️ CONFIGURAÇÃO TEMPORÁRIA DE TESTE ⚠️
+ *
+ * O parâmetro (exclude = {DataSourceAutoConfiguration.class}) abaixo
+ * serve apenas para rodar o projeto SEM Banco de Dados configurado.
+ *
+ * AÇÃO FUTURA: REMOVER o conteúdo entre parenteses quando
+ * o Banco de Dados for configurado oficialmente.
+ */
+@SpringBootApplication(exclude = { DataSourceAutoConfiguration.class })
 public class TialudeliverybackApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(TialudeliverybackApplication.class, args);
 	}
-
 }
