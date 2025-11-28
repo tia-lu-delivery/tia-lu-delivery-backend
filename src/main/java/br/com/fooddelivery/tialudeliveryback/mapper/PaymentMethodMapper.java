791bbedd4433f223d866a959556b641f92a9adae
HeyAlexOliveira <135489596+HeyAlexOliveira@users.noreply.github.com>
2025-11-28 14:59:27 -0300

    Refactor PaymentMethodMapper to use builder pattern

--- src/main/java/br/com/fooddelivery/tialudeliveryback/mapper/PaymentMethodMapper.java
+++ src/main/java/br/com/fooddelivery/tialudeliveryback/mapper/PaymentMethodMapper.java
@@ -2,27 +2,20 @@ package br.com.fooddelivery.tialudeliveryback.mapper;
 
 import br.com.fooddelivery.tialudeliveryback.dto.PaymentMethodResponseDTO;
 import br.com.fooddelivery.tialudeliveryback.entity.PaymentMethod;
-import br.com.fooddelivery.tialudeliveryback.enums.TipoCartao;
 
 public class PaymentMethodMapper {
 
     public static PaymentMethodResponseDTO toDTO(PaymentMethod entity) {
-
-        String numero = entity.getNumeroCartao();
-        String ultimosDigitos = numero.substring(numero.length() - 4);
-
-        String validadeMascarada = String.format(
-                "%02d/%02d",
-                entity.getValidadeMes(),
-                entity.getValidadeAno() % 100
-        );
-
-        return new PaymentMethodResponseDTO(
-                entity.getId(),
-                entity.getBandeira(),
-                ultimosDigitos,
-                entity.getTipoCartao().name(),
-                validadeMascarada
-        );
+        return PaymentMethodResponseDTO.builder()
+                .idMeioPagamento(entity.getId())
+                .status("atualizado")
+                .detalhe("Meio de pagamento atualizado com sucesso.")
+                .bandeira(entity.getBandeira())
+                .ultimosDigitos(entity.getUltimosDigitos())
+                .tipoCartao(entity.getTipoCartao().name())
+                .validade(String.format("%02d/%d",
+                        entity.getValidadeMes(),
+                        entity.getValidadeAno() % 100))
+                .build();
     }
 }
