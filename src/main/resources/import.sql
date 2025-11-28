66072042c64d61d5e5f0fb6d39ecd74991e98825
Denilson Santos <144867249+denilsonsantos-dev@users.noreply.github.com>
2025-11-27 21:59:05 -0300

    fix: preparação da aplicação para execução com banco de dados em memória
    O banco de dados H2 foi configurado para rodar dentro da build de desenvolvimento.
    
    O código SQL injeta valores padrões na base (o anterior causava erros durante a inicialização da aplicação, possivelmente porque o próprio app cria o banco de dados automaticamente).

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,14 +1,1 @@
-CREATE TABLE owner_partner (
-    id_socio VARCHAR(36) PRIMARY KEY,
-    cpf VARCHAR(11),
-    rg VARCHAR(20) NOT NULL,
-    orgao_emissor_rg VARCHAR(20) NOT NULL,
-    nome_completo VARCHAR(100) NOT NULL,
-    email VARCHAR(50) NOT NULL,
-    telefone VARCHAR(13) NOT NULL,
-    id_estabelecimento VARCHAR(36) NOT NULL
-);
-
-CREATE TABLE establishment (
-    id_estabelecimento VARCHAR(36) PRIMARY KEY
-)
+INSERT INTO establishment (id_estabelecimento) VALUES ('x9y8z7w6v5u4t3s2');
