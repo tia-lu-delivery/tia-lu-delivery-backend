cd1d4670ee10ef2d41ace43dcb903d05060afa51
HeyAlexOliveira <135489596+HeyAlexOliveira@users.noreply.github.com>
2025-11-28 15:05:13 -0300

    Update UnauthorizedException.java

--- src/main/java/br/com/fooddelivery/tialudeliveryback/exception/UnauthorizedException.java
+++ src/main/java/br/com/fooddelivery/tialudeliveryback/exception/UnauthorizedException.java
@@ -1,8 +1,16 @@
 package br.com.fooddelivery.tialudeliveryback.exception;
 
+import org.springframework.http.HttpStatus;
+
 public class UnauthorizedException extends RuntimeException {
+
+    private final HttpStatus status = HttpStatus.UNAUTHORIZED;
 
     public UnauthorizedException(String message) {
         super(message);
+    }
+
+    public HttpStatus getStatus() {
+        return status;
     }
 }
