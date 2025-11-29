2a3d6a867939b4e056173abe0f9ef42a40b5b464
HeyAlexOliveira <135489596+HeyAlexOliveira@users.noreply.github.com>
2025-11-28 21:56:29 -0300

    Update ProductRepository.java

--- src/main/java/br/com/fooddelivery/tialudeliveryback/repository/ProductRepository.java
+++ src/main/java/br/com/fooddelivery/tialudeliveryback/repository/ProductRepository.java
@@ -4,10 +4,13 @@ import br.com.fooddelivery.tialudeliveryback.entity.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.Optional;
 
 @Repository
 public interface ProductRepository extends JpaRepository<Product, Long> {
-    Optional<Product> findByRestaurantIdAndId(Long restaurantId, Long id);
-}
 
+    Optional<Product> findByRestaurantIdAndId(Long restaurantId, Long id);
+
+    List<Product> findByRestaurantId(Long restaurantId);
+}
