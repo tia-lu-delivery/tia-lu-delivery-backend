6a5ef4e6e599708fe694862d50aebf2fb26fb5a5
Bryan Ferraz <bryan.ferraz@hotmail.com>
2025-12-11 19:12:02 -0300

    style: format main method for consistent indentation

--- src/main/java/br/com/fooddelivery/tialudeliveryback/TialudeliverybackApplication.java
+++ src/main/java/br/com/fooddelivery/tialudeliveryback/TialudeliverybackApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class TialudeliverybackApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(TialudeliverybackApplication.class, args);
-	}
+  public static void main(String[] args) {
+    SpringApplication.run(TialudeliverybackApplication.class, args);
+  }
 
 }
