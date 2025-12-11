f0b21045b7a531d20336855bd4466addda96eea6
Bryan Ferraz <bryan.ferraz@hotmail.com>
2025-12-11 19:12:12 -0300

    feat: update UserRepository to use UserEntity and correct generic types

--- src/main/java/br/com/fooddelivery/tialudeliveryback/repository/UserRepository.java
+++ src/main/java/br/com/fooddelivery/tialudeliveryback/repository/UserRepository.java
@@ -1,15 +1,15 @@
 package br.com.fooddelivery.tialudeliveryback.repository;
 
-import br.com.fooddelivery.tialudeliveryback.model.User;
+import br.com.fooddelivery.tialudeliveryback.entity.UserEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
 @Repository
-public interface UserRepository extends JpaRepository<User, Long> {
+public interface UserRepository extends JpaRepository<UserEntity, String> {
 
-  Optional<User> findByEmail(String email);
+  Optional<UserEntity> findByEmail(String email);
 
   boolean existsByEmail(String email);
 }
