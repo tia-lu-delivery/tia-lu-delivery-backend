04b6eff7ea828334748296113eadc031782b8f36
Bryan Ferraz <bryan.ferraz@hotmail.com>
2025-12-11 19:12:06 -0300

    feat: implement SecurityConfig to manage HTTP security settings

---
+++ src/main/java/br/com/fooddelivery/tialudeliveryback/config/SecurityConfig.java
@@ -0,0 +1,22 @@
+package br.com.fooddelivery.tialudeliveryback.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+  @Bean
+  SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+    http
+        .csrf(csrf -> csrf.disable())
+        .authorizeHttpRequests(auth -> auth
+            .requestMatchers("/api/v1/users/**").permitAll()
+            .anyRequest().permitAll());
+    return http.build();
+  }
+}
